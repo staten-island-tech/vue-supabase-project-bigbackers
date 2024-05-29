c995c4f0b0491e4dd750ac895ad2dda95779c9d5
lbernice <bernicel677@gmail.com>
2024-05-28 23:12:18 -0400

    x

--- supabase/src/components/Auth.vue
+++ supabase/src/components/Auth.vue
@@ -1,19 +1,22 @@
 <template>
-  <form class="row flex-center flex" @submit.prevent="handleLogin">
-    <div class="col-6 form-widget">
-      <h1 class="header">Supabase + Vue 3</h1>
-      <p class="description">Sign in with your email below</p>
-      <div>
-        <input class="inputField" required type="email" placeholder="Your email" v-model="email" />
-      </div>
-      <div>
-        <input
-          type="submit"
-          class="button block"
-          :value="loading ? 'Loading' : 'Submit'"
-          :disabled="loading"
-        />
-      </div>
+<form class="row flex-center flex" @submit.prevent="handleLogin">
+  <div class="col-6 form-widget">
+    <h1 class="header">Login</h1>
+    <p class="description">Sign in with your email below</p>
+  <div>
+      <input class="email" required type="email" placeholder="Email" v-model="email" />
+  </div>
+  <div>
+    <input class="password" required type="password" placeholder="Password" v-model="password" id="password" />
+  </div>
+    <div>
+      <input
+        type="submit"
+        class="button block"
+        :value="loading ? 'Loading' : 'Submit'"
+        :disabled="loading"
+      />
+    </div>
     </div>
   </form>
 </template>

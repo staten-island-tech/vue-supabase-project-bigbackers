f632b3f9fc0e164f2a5aa07badafe87414556196
Bernice <bernicel677@gmail.com>
2024-06-01 18:20:10 -0400

    change db

--- supabase/src/supabase.js
+++ supabase/src/supabase.js
@@ -1,6 +1,6 @@
+
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ycxlxueqwsqdcavdjrhu.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljeGx4dWVxd3NxZGNhdmRqcmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzMTA1MzEsImV4cCI6MjAzMTg4NjUzMX0.RA4mmbYiTTe8fvpMVwVGKcHpLawTTbm82alIlvNJd74'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabaseUrl = 'https://astvsyfxlyansxfbtfos.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)

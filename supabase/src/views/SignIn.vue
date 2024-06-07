ca6674ad404a34d2767f85740af306326c64a427
lbernice <bernicel677@gmail.com>
2024-06-07 02:21:19 -0400

    css

--- supabase/src/views/SignIn.vue
+++ supabase/src/views/SignIn.vue
@@ -2,9 +2,9 @@
     <div>
         <h2>Sign In</h2>
         <form @submit.prevent="signIn">
-            <input v-model="email" type="email" placeholder="Email" required />
-            <input v-model="password" type="password" placeholder="Password" required />
-            <button type="submit">Sign In</button>
+            <input v-model="email" type="email" placeholder="Email" required class="email"/>
+            <input v-model="password" type="password" placeholder="Password" required class="password"/>
+            <button type="submit" class="signin">Sign In</button>
         </form>
         <p>{{ error }}</p>
         <p>Don't have an account? <router-link to="/signup">Sign Up</router-link></p>
@@ -46,5 +46,13 @@ export default {
 </script>
 
 <style scoped>
-
+.email{
+  font-size: 1rem;
+}
+.password{
+  font-size: 1rem;
+}
+.signin{
+  font-size: 1rem;
+}
 </style>

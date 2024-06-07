34c256a71feb1b94b49663198c305ceae80cb766
lbernice <bernicel677@gmail.com>
2024-06-07 01:15:48 -0400

    x

--- supabase/src/views/Welcome.vue
+++ supabase/src/views/Welcome.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-
+    <h1>Your To-Do List</h1>
+    <div v-if="user">
+      <div>
+        <h2>Welcome, {{ user.email }}</h2>
+        <button @click="logout">Logout</button>
+      </div>
+    </div>
   </div>
 </template>
 

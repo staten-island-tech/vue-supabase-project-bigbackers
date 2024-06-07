2b12ff0e4282919d746470ed6dd17ffb0af68752
lbernice <bernicel677@gmail.com>
2024-06-07 00:28:31 -0400

    signup

--- supabase/src/views/SignUp.vue
+++ supabase/src/views/SignUp.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-
+    <h2>Sign Up</h2>
+    <form @submit.prevent="signUp">
+      <input v-model="email" type="email" placeholder="Email" required />
+      <input v-model="password" type="password" placeholder="Password" required />
+      <button type="submit">Sign Up</button>
+    </form>
+    <p>{{ error }}</p>
+    <p>Already have an account? <router-link to="/signin">Sign In</router-link></p>
   </div>
 </template>
 
